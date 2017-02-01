8a3848aec11b1b58ba1a90ef947ce37b3c9a0084
Tanmay Patil <tanmay.ashok.patil@gmail.com>
2017-02-01 01:42:41 -0700

    Perf improv: Added connection pool to the db

--- MainApp/lib/index.js
+++ MainApp/lib/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
-import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import path from 'path';
 import api from './app/routes/api';
+import connectDB from './app/helpers/db';
 
 const app = express();
 const port = process.env.PORT || 8080; // TODO move to constants
@@ -16,14 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(express.static(__dirname + '/../public'));
 app.use('/api', appRoutes);
-
-mongoose.connect('mongodb://localhost:27017/hospital', function(err){
-	if(err){
-		console.log("Not connecting to mongo: " + err);
-	} else{
-	console.log("Successfully connected to mongo");
-	}
-});
+connectDB();
 
 app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/../public/app/views/index.html'));
