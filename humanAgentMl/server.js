0cb5f0a3ba92f44f848f77df49606bde2664a149
adityab17190 <abivalka@asu.edu>
2017-02-02 20:01:43 -0700

    1) google-signin.html- Admin Login Page with Gmail sign-in button  2)server.js- Added route for login page and REST api to check admin 3)admins-controller.js- Added api to check whether username exists in admins table

--- humanAgentMl/server.js
+++ humanAgentMl/server.js
@@ -12,11 +12,18 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/client/views/index.html');
 });
 
+app.get('/login',function(req,res){
+	res.sendfile(__dirname + '/client/views/google-Login.html');
+}
+);
+
 app.use('/js', express.static(__dirname + '/client/js'));
 
 //REST API
 app.get('/api/admins', adminsController.list);
 app.post('/api/admins', adminsController.create);
+app.post('/api/adminLogin',adminsController.checkAdmin);
+
 
 app.listen(3000, function() {
   console.log('I\'m Listening...');
