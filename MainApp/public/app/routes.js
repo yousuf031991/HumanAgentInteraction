996063acd19e1def202de2c23823b5720ed96cdd
Tanmay Patil <tanmay.ashok.patil@gmail.com>
2017-02-03 18:41:46 -0700

    Angular based login-workflow

--- MainApp/public/app/routes.js
+++ MainApp/public/app/routes.js
@@ -1,12 +1,15 @@
 angular.module('appRoutes', ['ngRoute']).config(function ($routeProvider, $locationProvider) {
     $routeProvider.when('/', {
-        templateUrl: 'app/views/pages/home.html', controller: 'signInCtrl', controllerAs: 'signIn'
+            templateUrl: 'app/views/pages/home.html', controller: 'signInCtrl', controllerAs: 'signIn'
         })
         .when('/about', {
             templateUrl: 'app/views/pages/about.html'
         })
         .when('/adminpage', {
             templateUrl: 'app/views/pages/admin/adminPage.html'
+        })
+        .when('/adminlogin', {
+            templateUrl: 'app/views/pages/authentication/login.html'
         })
         .when('/trialinfo', {
             templateUrl: 'app/views/pages/trialinfo/trialInfoPage.html' , controller: 'trialInfoCtrl', controllerAs: 'trialData'
