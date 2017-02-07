0daf54836b039ed3ef29ac22e671c8748e98f640
aditya <aditya>
2017-02-06 23:57:28 -0700

    Created a new service verify to check whether username exists in admin table

--- MainApp/public/app/services/adminServices.js
+++ MainApp/public/app/services/adminServices.js
@@ -2,8 +2,12 @@ angular.module('adminServices', [])
     .factory('Admin', function ($http) {
         adminFactory = {};
         adminFactory.create = function(signInData){
-        	console.log("In adminservices");
-            return $http.post('/api/adminLogin', signInData);
+                return $http.post('/api/admin', signInData);
         };
+
+        adminFactory.verify=function(signInData){
+                return $http.post('/api/adminLogin', signInData);
+        }; 
+
         return adminFactory;
     });
