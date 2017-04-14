419cdb3b5fa5926568a036669d43da23d017c7b5
tgomudur@asu.edu <tgomudur@asu.edu>
2017-04-13 18:09:36 -0700

    Changed case in NODE_ENV

--- MainApp/run.sh
+++ MainApp/run.sh
@@ -1,1 +1,1 @@
-mongod --dbpath=/media/hospmanagement/My\ Book/MachineLearning/HumanAgentInteraction/MainApp/database> /dev/null & npm run build && NODE_ENV=PRODUCTION pm2 start dist/index.js
+mongod --dbpath=/media/hospmanagement/My\ Book/MachineLearning/HumanAgentInteraction/MainApp/database> /dev/null & npm run build && NODE_ENV=production pm2 start dist/index.js
