fd057fc138f1bb698ae8d3a0528ab5c87a1c82e0
tgomudur@asu.edu <tgomudur@asu.edu>
2017-04-26 23:15:51 -0700

    Modified run.sh to kill previous pm2 instance of the game

--- MainApp/run.sh
+++ MainApp/run.sh
@@ -1,1 +1,4 @@
-mongod --dbpath=/media/hospmanagement/My\ Book/MachineLearning/HumanAgentInteraction/MainApp/database> /dev/null & npm run build && NODE_ENV=production pm2 start dist/index.js
+#!/usr/bin/env bash
+pm2 delete hospgame && git pull origin master &&
+mongod --dbpath=/media/hospmanagement/My\ Book/MachineLearning/HumanAgentInteraction/MainApp/database> /dev/null &
+npm run build && NODE_ENV=production pm2 start dist/index.js --name hospgame
